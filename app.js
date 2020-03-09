f21dc0db4bf37cef9d9802fb41ba3f38dc1b6641
Neel Patel <neelpatel.6531@gmail.com>
2020-03-09 15:12:10 +0530

    data call

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const eventList = require("./eventList");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var port = process.env.PORT || 1337;
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public", { maxAge: 31557600000 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/getData", async (req, res) => {
+  var resData = {
+    dateTime: "",
+    aqi: "",
+    event: "",
+    weather: "",
+    temperatureHigh: "",
+    temperatureLow: "",
+    temperature: ""
+  };
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  axios.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  axios.get('https://api.waqi.info/feed/ahmedabad/?token=7469034730f551142051aae18bc9e375daa5d8a8')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  axios.get('https://api.darksky.net/forecast/ba58d351c00fd5c6fdb21e13a5c02c24/22.9852618,72.6321034')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  console.log("getData");
+  res.send("getData");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+var httpServer = require("http").createServer(app);
+httpServer.listen(port);
 
 module.exports = app;
